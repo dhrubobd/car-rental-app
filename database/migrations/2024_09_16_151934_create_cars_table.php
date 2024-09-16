4bef8084a2523f5d5083c456ade5b1748e071ae0
Luke Kalyan Brata Sarker <dhrubo.luke@gmail.com>
2024-09-16 22:07:25 +0600

    Cars Table Migration work is done.

--- database/migrations/2024_09_16_151934_create_cars_table.php
+++ database/migrations/2024_09_16_151934_create_cars_table.php
@@ -13,7 +13,16 @@ return new class extends Migration
     {
         Schema::create('cars', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name',30);
+            $table->string('brand',30);
+            $table->string('model',30);
+            $table->integer('year');
+            $table->string('car_type',30);
+            $table->decimal('daily_rent_price', total: 6, places: 2);
+            $table->boolean('availability');
+            $table->string('image');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrentOnUpdate();
         });
     }
 
