d27ad6223946fb6740474768c707dfa8fa9593f8
Luke Kalyan Brata Sarker <dhrubo.luke@gmail.com>
2024-09-17 10:40:54 +0600

    Users are created using seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,37 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        DB::table('users')->insert([
+            [
+                'name' => 'Harry Potter',
+                'email' => Str::random(10).'@example.com',
+                'password' => Str::random(7),
+                'role' => 'admin',
+            ],
+            [
+                'name' => 'Hasnat Abdullah',
+                'email' => Str::random(10).'@example.com',
+                'password' => Str::random(7),
+                'role' => 'customer',
+            ],
+            [
+                'name' => 'Dipty Chowdhruy',
+                'email' => Str::random(10).'@example.com',
+                'password' => Str::random(7),
+                'role' => 'customer',
+            ],
+            [
+                'name' => 'Sarjis Alom',
+                'email' => Str::random(10).'@example.com',
+                'password' => Str::random(7),
+                'role' => 'customer',
+            ],
+            [
+                'name' => 'Sadia Sraboni',
+                'email' => Str::random(10).'@example.com',
+                'password' => Str::random(7),
+                'role' => 'customer',
+            ],
+        ]);
     }
 }
